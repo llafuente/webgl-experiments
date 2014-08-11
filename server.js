9f9fc167318ce82e438a426e0293b7320eb2be83
llafuente <llafuente@noboxout.com>
2014-08-11 20:52:01 +0200

    basic save scene and brush support

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var http = require('http');
 
 var express = require('express');
+var multer = require('multer');
 var serveStatic = require('serve-static')
 var app = express();
 var server = http.createServer(app);
@@ -11,6 +12,18 @@ app.use("/", serveStatic(__dirname + "/"));
 
 // Start up the server on the port specified in the config
 server.listen(port, '0.0.0.0', port, function() {
+});
+
+app.use(multer({
+    dest: './uploads/',
+    rename: function (fieldname, filename) {
+        return filename.replace(/\W+/g, '-').toLowerCase() + Date.now()
+    }
+}));
+
+app.post('/upload', function (req, res) {
+    console.log(req.files);
+    res.send({success:true, files: req.files});
 });
 
 console.log('Server - listening on port: ' + port);
